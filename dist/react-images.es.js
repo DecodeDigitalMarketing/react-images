2498be15a87976b8e8c6d51c996f8f924f91a9ee
Daniel Dyer <danielbdyer@gmail.com>
2020-12-17 07:06:12 -0600

    Forgot to import paginator count into header component properly

--- dist/react-images.es.js
+++ dist/react-images.es.js
@@ -395,22 +395,6 @@ var Footer = function Footer(props) {
             fill: '#fff'
           })
         )
-      ),
-      glam(
-        'a',
-        {
-          href: 'https://www.youtube.com/user/CHIStLukesHealth',
-          target: '_blank',
-          rel: 'noreferrer'
-        },
-        glam(
-          'svg',
-          { height: '50px', viewBox: '0 0 24 24', width: '24' },
-          glam('path', {
-            d: 'm19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993z',
-            fill: '#fff'
-          })
-        )
       )
     )
   );
@@ -583,7 +567,7 @@ var Header = function Header(props) {
       toggleFullscreen = modalProps.toggleFullscreen;
 
   var FsIcon = isFullscreen ? FullscreenExit : FullscreenEnter;
-  var Count = components.Count,
+  var FooterCount = components.FooterCount,
       CloseButton = components.CloseButton,
       FullscreenButton = components.FullscreenButton;
 
@@ -603,7 +587,7 @@ var Header = function Header(props) {
     glam(
       'span',
       null,
-      glam(Count, props)
+      glam(FooterCount, props)
     ),
     glam(
       'span',
